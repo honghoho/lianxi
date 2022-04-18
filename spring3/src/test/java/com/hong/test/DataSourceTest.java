692dff8ab08183f095cfc394164aeb0ae4b31ea4
honghoho <564289211@qq.com>
2022-04-18 21:28:46 +0800

    4.18

---
+++ spring3/src/test/java/com/hong/test/DataSourceTest.java
@@ -0,0 +1,23 @@
+package com.hong.test;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.testng.annotations.Test;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+
+public class DataSourceTest {
+    @Test
+    //测试手动创建c3p0数据源
+    public void test1() throws Exception {
+        ComboPooledDataSource dataSource =new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/user");
+        dataSource.setUser("root");
+        dataSource.setPassword("123456");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+}
