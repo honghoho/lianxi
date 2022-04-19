ee3f944bb9ea00f965e8f04764564887bfee9086
honghoho <564289211@qq.com>
2022-04-19 20:04:22 +0800

    4.19

--- spring3/src/test/java/com/hong/test/DataSourceTest.java
+++ spring3/src/test/java/com/hong/test/DataSourceTest.java
@@ -11,8 +11,8 @@ public class DataSourceTest {
     //测试手动创建c3p0数据源的
     public void test1() throws Exception {
         ComboPooledDataSource dataSource =new ComboPooledDataSource();
-        dataSource.setDriverClass("com.mysqljdbc.Driver");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/user");
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/spring?serverTimezone=UTC");
         dataSource.setUser("root");
         dataSource.setPassword("123456");
         Connection connection = dataSource.getConnection();
